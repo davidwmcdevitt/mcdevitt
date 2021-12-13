7657db4e829fcfe9a19d7077ace7ec1d5700647f
davidwmcdevitt <31931152+davidwmcdevitt@users.noreply.github.com>
2021-12-12 21:21:43 -0600

    Add files via upload

---
+++ PitchforkBot/app.R
@@ -0,0 +1,136 @@
+library(shiny)
+library(tidyverse)
+library(spotifyr)
+library(tidytext)
+library(extrafont)
+library(dplyr)
+library(knitr)
+library(shinybusy)
+
+Sys.setenv(SPOTIFY_CLIENT_ID = "0e6eb928dff94031aac094c8c65181f4")
+Sys.setenv(SPOTIFY_CLIENT_SECRET = "e452221800314c08a5d9e7739b276779")
+
+access_token <- get_spotify_access_token()
+
+ui = basicPage(
+  textInput("url", "Enter the text to display below:"),
+  actionButton("goButton", "Go!", class = "btn-success"),
+  verbatimTextOutput("verb")
+)
+server = function(input, output) {
+  observeEvent(input$goButton, {
+    
+    show_modal_spinner() # show the modal window
+    
+    
+    Sys.setenv(SPOTIFY_CLIENT_ID = "0e6eb928dff94031aac094c8c65181f4")
+    Sys.setenv(SPOTIFY_CLIENT_SECRET = "e452221800314c08a5d9e7739b276779")
+    
+    access_token <- get_spotify_access_token()
+    
+    url <- input$url
+    uri <- substring(url,regexpr("playlist/", url)+9)
+    uri <-substring(uri, 1,regexpr("=",uri)-4)
+    
+    
+    temp <- get_playlist(uri)
+    
+    playlists <- get_playlist_tracks(uri)
+    
+    len <- ceiling(temp$tracks$total / 100)
+    
+    for (l in 2:len) {
+      offset <- l*100 - 100 
+      temp <- get_playlist_tracks(uri, offset = offset)
+      playlists <- rbind(playlists, temp)
+    }
+    
+    tracks <- subset(playlists, select = c("track.name","track.artists","track.album.name"))
+    tracks$artist <- NA
+    
+    z <- nrow(tracks)
+    
+    artists <- c()
+    
+    for (i in 1:z) {
+      temp <- tracks[[2]][[i]]$name[1]
+      artists <- c(artists, temp)
+    }
+    
+    tracks$artist <- artists
+    tracks$track.artists <- NULL
+    
+    tracks <- rename(tracks, name= track.name, album = track.album.name)
+    
+    # Clean and Guess URL
+    
+    tracks$artist <- tolower(tracks$artist)
+    tracks$artist <- gsub("/", "-",tracks$artist)
+    tracks$artist <- gsub(":", "",tracks$artist)
+    tracks$artist <- gsub("&", "and",tracks$artist)
+    tracks$artist <- gsub("-", " ",tracks$artist)
+    tracks$artist <- gsub("[[:punct:]]", "",tracks$artist)
+    tracks$artist <- gsub("'", "",tracks$artist)
+    tracks$artist <- gsub(" ", "-",tracks$artist)
+    
+    tracks$album <- tolower(tracks$album)
+    tracks$album <- gsub("/", "-",tracks$album)
+    tracks$album <- gsub(":", "",tracks$album)
+    tracks$album <- gsub("&", "and",tracks$album)
+    tracks$album <- gsub("-", " ",tracks$album)
+    tracks$album <- gsub("[[:punct:]]", "",tracks$album)
+    tracks$album <- gsub("'", "",tracks$album)
+    tracks$album <- gsub(" ", "-",tracks$album)
+    
+    tracks$url_guess <- paste("https://pitchfork.com/reviews/albums/",tracks$artist,"-",tracks$album, sep = "")
+    
+    url_list <- tracks$url_guess
+    
+    content <- data.frame()
+    
+    for (x in url_list) {
+      text <- paste(x)
+      url <- text
+      
+      try(webpage <- read_html(url), silent = TRUE)
+      
+      if (exists("webpage") != FALSE ) {
+        review_score_html <- html_nodes(webpage, '.score')
+        review_genre_html <- html_nodes(webpage, '.genre-list__item')
+        review_text_html  <- html_nodes(webpage, 'p')
+        
+        if(length(review_genre_html) == 0){
+          review_genre <- "No Genre"
+        }
+        
+        if(length(review_genre_html) >= 1){
+          review_genre <- html_text(review_genre_html)
+        }
+        
+        review_score <- html_text(review_score_html)
+        review_text <- html_text(review_text_html)
+        temp <- list()
+        for (i in 1:length(review_text)) {
+          temp <- paste(temp, review_text[i])   
+        }
+        review_text <- temp
+        
+        url_string <- x
+        
+        review_content <- data.frame(url_string, review_genre, review_score, review_text)
+        content <- rbind(content, review_content)
+        remove(webpage)
+      }
+      
+    } 
+    content <- unique(content)
+    mean <- round(mean(as.numeric(content$review_score)),digits = 1)
+    text <- paste("Pitchfork gives this playlist a", mean, sep = " ")
+    
+    
+    remove_modal_spinner() # remove it when done
+    
+  output$verb <- renderText({ text })
+  })
+}
+shinyApp(ui, server)
